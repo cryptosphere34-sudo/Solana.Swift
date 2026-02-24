fffe218f7a3baf31b3ccc539707e6cb33b92e2ca
Nighty Knight Coding <nightyknightgaming@gmail.com>
2026-02-23 19:40:54 -0800

    Fix append type mismatch in Array+Extension init(hex:)

--- Sources/Solana/Extensions/Array+Extension.swift
+++ Sources/Solana/Extensions/Array+Extension.swift
@@ -14,54 +14,42 @@ extension Array {
 }
 
 extension Array where Element == UInt8 {
-  public init(hex: String) {
-    self.init(reserveCapacity: hex.unicodeScalars.lazy.underestimatedCount)
-    var buffer: UInt8?
-    var skip = hex.hasPrefix("0x") ? 2 : 0
-    for char in hex.unicodeScalars.lazy {
-      guard skip == 0 else {
-        skip -= 1
-        continue
-      }
-      guard char.value >= 48 && char.value <= 102 else {
-        removeAll()
-        return
-      }
-      let v: UInt8
-      let c: UInt8 = UInt8(char.value)
-      switch c {
-        case let c where c <= 57:
-          v = c - 48
-        case let c where c >= 65 && c <= 70:
-          v = c - 55
-        case let c where c >= 97:
-          v = c - 87
-        default:
-          removeAll()
-          return
-      }
-      if let b = buffer {
-        append(b << 4 | v)
-        buffer = nil
-      } else {
-        buffer = v
-      }
-    }
-    if let b = buffer {
-      append(b)
-    }
-  }
+    public init(hex: String) {
+        var buffer: UInt8? = nil
+        let hexicodeScalars = hex.unicodeScalars.lazy.underestimatedCount
 
-  public func toHexString() -> String {
-    `lazy`.reduce(into: "") {
-      var s = String($1, radix: 16)
-      if s.count == 1 {
-        s = "0" + s
-      }
-      $0 += s
+        self = Array(reserveCapacity: hexicodeScalars / 2 + 1)
+
+        var skip = hex.hasPrefix("0x") ? 2 : 0
+        for char in hex.unicodeScalars.lazy {
+            guard skip == 0 else {
+                skip -= 1
+                continue
+            }
+
+            guard char.value >= 48 && char.value <= 102 else { continue }
+
+            let v: UInt8
+            switch UInt8(char.value) {
+            case 48...57: v = UInt8(char.value) - 48
+            case 65...70: v = UInt8(char.value) - 55
+            case 97...102: v = UInt8(char.value) - 87
+            default: continue
+            }
+
+            if let b = buffer {
+                let combined = UInt8((UInt16(b) << 4) | UInt16(v))
+                append(combined)
+                buffer = nil
+            } else {
+                buffer = v
+            }
+        }
+
+        if let b = buffer {
+            append(b)
+        }
     }
-  }
-}
 
 public extension Array where Element == UInt8 {
   func toBase64() -> String {
